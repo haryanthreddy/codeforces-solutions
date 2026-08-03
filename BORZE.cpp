5743f17f9d570a8b17ea12f422a7ed73d8aba97f
Haryanth reddy <f20260143@hyderabad.bits-pilani.ac.in>
2026-08-03 19:24:48 +0530

    add codeforces solutions borze.cpp

---
+++ BORZE.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    for (int i=0;i<s.size();)
+    {
+        if (s[i] == '.')
+        {
+            cout<<0;
+            i++;
+        }
+        else if(s[i] == '-'&& s[i+1]== '.')
+        {
+            cout<<1;
+            i+=2;
+        }
+        else{
+            cout<<2;
+            i+=2;
+        }
+    }
+    return 0;
+}
