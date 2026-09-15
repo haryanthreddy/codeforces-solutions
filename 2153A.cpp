284a470c994d4994960de0c7e02e13b50d006f16
Haryanth reddy <f20260143@hyderabad.bits-pilani.ac.in>
2026-09-15 11:45:39 +0530

    add codeforces solutions

---
+++ 2153A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n;
+    cin>>n;
+    set <int> s;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        s.insert(x);
+    }
+    cout<<s.size()<<"\n";
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
